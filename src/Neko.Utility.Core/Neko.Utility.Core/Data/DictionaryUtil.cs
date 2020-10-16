a8bfa6d5eeb36ae4af81bbf13849d34606a63625
SKYC829 <13677727680@163.com>
2020-10-16 16:05:41 +0800

    新增将对象转换为字典的方法

--- src/Neko.Utility.Core/Neko.Utility.Core/Data/DictionaryUtil.cs
+++ src/Neko.Utility.Core/Neko.Utility.Core/Data/DictionaryUtil.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
 
 namespace Neko.Utility.Core.Data
 {
@@ -48,11 +51,81 @@ namespace Neko.Utility.Core.Data
             {
                 try
                 {
-                    result = (Tvalue)Convert.ChangeType(dictionary[key], typeof(Tvalue));
+                    result = (Tvalue)System.Convert.ChangeType(dictionary[key], typeof(Tvalue));
                 }
                 catch (Exception ex)
                 {
                     //TODO:
+                }
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// 将一个对象<see cref="object"/>转换为字典
+        /// <para>当<see cref="object"/>为类,且类包含方法时,不建议转换为字典</para>
+        /// </summary>
+        /// <param name="target">要转换的对象</param>
+        /// <returns></returns>
+        public static IDictionary Convert(object target)
+        {
+            IDictionary result = null;
+            try
+            {
+                if(target is IDictionary)
+                {
+                    result = target as IDictionary;
+                }
+                else
+                {
+                    string dicJson = JsonConvert.SerializeObject(target);
+                    result = JsonConvert.DeserializeObject<IDictionary>(dicJson);
+                }
+            }
+            finally
+            {
+                if(result == null)
+                {
+                    result = new Dictionary<string, object>();
+                    Type targetType = target.GetType();
+                    if(target is JObject)
+                    {
+                        JObject jObject = target as JObject;
+                        foreach (KeyValuePair<string,JToken> token in jObject)
+                        {
+                            string key = token.Key;
+                            if(token.Value is JValue value)
+                            {
+                                result[key] = value.Value;
+                            }
+                            else
+                            {
+                                result[key] = null;
+                            }
+                        }
+                    }
+                    else if (targetType.IsClass)
+                    {
+                        PropertyInfo[] properties = targetType.GetProperties();
+                        foreach (PropertyInfo property in properties)
+                        {
+                            if (!property.CanRead)
+                            {
+                                continue;
+                            }
+                            result[property.Name] = property.GetValue(target);
+                        }
+                        FieldInfo[] fields = targetType.GetFields();
+                        foreach (FieldInfo field in fields)
+                        {
+                            string fieldName = field.Name;
+                            if (result.Contains(fieldName))
+                            {
+                                fieldName = string.Format("fi_{0}", fieldName);
+                            }
+                            result[fieldName] = field.GetValue(target);
+                        }
+                    }
                 }
             }
             return result;
