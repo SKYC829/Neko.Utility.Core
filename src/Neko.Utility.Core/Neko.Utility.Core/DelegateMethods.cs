81553a4314f0f08128a7c66b52c185f1d54f0480
SKYC829 <13677727680@163.com>
2020-10-22 14:42:00 +0800

    定义一个无参的委托方法

---
+++ src/Neko.Utility.Core/Neko.Utility.Core/DelegateMethods.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Neko.Utility.Core
+{
+    /// <summary>
+    /// 无参的委托方法
+    /// </summary>
+    public delegate void EmptyDelegateCode();
+}
