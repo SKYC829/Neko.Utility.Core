4c68cb37a811c3f93c74a13b11969472a8fd488d
SKYC829 <13677727680@163.com>
2020-10-26 14:57:53 +0800

    新增无参有返回值的委托方法和有参无返回值的委托方法

--- src/Neko.Utility.Core/Neko.Utility.Core/DelegateMethods.cs
+++ src/Neko.Utility.Core/Neko.Utility.Core/DelegateMethods.cs
@@ -8,4 +8,18 @@ namespace Neko.Utility.Core
     /// 无参的委托方法
     /// </summary>
     public delegate void EmptyDelegateCode();
+
+    /// <summary>
+    /// 无参的委托方法
+    /// </summary>
+    /// <typeparam name="TReturn">返回值类型</typeparam>
+    /// <returns></returns>
+    public delegate TReturn EmptyDelegateCode<TReturn>();
+
+    /// <summary>
+    /// 有参的委托方法
+    /// </summary>
+    /// <typeparam name="T">参数类型</typeparam>
+    /// <param name="args">参数</param>
+    public delegate void ParameterDelegateCode(params object[] args);
 }
