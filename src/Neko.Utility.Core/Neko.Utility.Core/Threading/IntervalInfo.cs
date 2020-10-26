a5f6663af30f24af78d1178fa78fe63a0169b483
SKYC829 <13677727680@163.com>
2020-10-26 17:00:08 +0800

    Create IntervalInfo.cs
    线程信息的实体类对象,用于启用循环线程时控制循环流程

---
+++ src/Neko.Utility.Core/Neko.Utility.Core/Threading/IntervalInfo.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading;
+
+namespace Neko.Utility.Core.Threading
+{
+    /// <summary>
+    /// 线程信息的实体类对象
+    /// </summary>
+    public class IntervalInfo
+    {
+        /// <summary>
+        /// 当前线程
+        /// </summary>
+        public Thread CurrentThread { get; set; }
+
+        /// <summary>
+        /// 线程循环执行时的间隔时间（单位:毫秒）
+        /// </summary>
+        public int Interval { get; set; }
+
+        /// <summary>
+        /// 线程要执行的方法
+        /// </summary>
+        public EmptyDelegateCode ExecuteCode { get; set; }
+
+        /// <summary>
+        /// 是否中断
+        /// </summary>
+        public bool Break { get; set; }
+    }
+}
