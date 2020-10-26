f1f3ffb9d3e51d3b19ab36de5f7c0c4062ff2b4e
SKYC829 <13677727680@163.com>
2020-10-26 15:00:09 +0800

    新增日志信息实体类(内部类)

---
+++ src/Neko.Utility.Core/Neko.Utility.Core/IO/Logging/LogInfo.cs
@@ -0,0 +1,57 @@
+﻿using Neko.Utility.Core.Configurations;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Neko.Utility.Core.IO.Logging
+{
+    /// <summary>
+    /// 日志信息实体类
+    /// </summary>
+    [Serializable]
+    internal class LogInfo
+    {
+        /// <summary>
+        /// 记录日志的时间
+        /// </summary>
+        public DateTime LogTime { get; set; }
+
+        /// <summary>
+        /// 记录日志的信息
+        /// </summary>
+        public string LogMessage { get; set; }
+
+        /// <summary>
+        /// 记录的异常信息
+        /// </summary>
+        public Exception InnerException { get; set; }
+
+        /// <summary>
+        /// 短时间内同一日志出现的次数
+        /// </summary>
+        public int LogCount { get; set; }
+
+        /// <summary>
+        /// 日志等级
+        /// </summary>
+        public LogLevel LogLevel { get; set; }
+
+        public LogInfo(LogLevel logLevel, DateTime logTime, string logMessage) : this(logLevel, logTime, logMessage, null)
+        {
+
+        }
+
+        public LogInfo(LogLevel logLevel, DateTime logTime, Exception innerException) : this(logLevel, logTime, innerException.Message, innerException)
+        {
+
+        }
+
+        public LogInfo(LogLevel logLevel, DateTime logTime, string logMessage, Exception innerException)
+        {
+            LogLevel = logLevel;
+            LogTime = logTime;
+            LogMessage = logMessage;
+            InnerException = innerException;
+        }
+    }
+}
