02a30637934edaaa6bbe40d38798e93a5925f724
SKYC829 <13677727680@163.com>
2020-10-26 14:59:44 +0800

    新增日志等级枚举

---
+++ src/Neko.Utility.Core/Neko.Utility.Core/Configurations/LogLevel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Neko.Utility.Core.Configurations
+{
+    /// <summary>
+    /// 日志等级
+    /// </summary>
+    [Serializable]
+    public enum LogLevel
+    {
+        /// <summary>
+        /// 步骤日志,一般用于开发
+        /// </summary>
+        Track,
+        /// <summary>
+        /// 普通信息日志
+        /// </summary>
+        Information,
+        /// <summary>
+        /// 警告信息日志
+        /// </summary>
+        Warning,
+        /// <summary>
+        /// 异常信息日志
+        /// </summary>
+        Exception
+    }
+}
