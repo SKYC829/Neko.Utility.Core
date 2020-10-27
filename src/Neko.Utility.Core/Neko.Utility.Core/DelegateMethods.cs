850afd0bb83fda4ab0c85cb6a30b766f05bfc99e
SKYC829 <13677727680@163.com>
2020-10-27 17:41:56 +0800

    Update DelegateMethods.cs
    新增压缩文件时用到的委托方法

--- src/Neko.Utility.Core/Neko.Utility.Core/DelegateMethods.cs
+++ src/Neko.Utility.Core/Neko.Utility.Core/DelegateMethods.cs
@@ -17,4 +17,13 @@
     /// </summary>
     /// <param name="args">参数</param>
     public delegate void ParameterDelegateCode(params object[] args);
+
+    /// <summary>
+    /// 压缩/解压文件时的委托方法
+    /// </summary>
+    /// <param name="fileName">正在压缩/解压的文件</param>
+    /// <param name="compressedSize">已经压缩/解压的文件大小</param>
+    /// <param name="totalSize">正在压缩/解压的文件总大小</param>
+    /// <param name="isFolder">当前是否是文件夹</param>
+    public delegate void CompressDelegateCode(string fileName, uint compressedSize, long totalSize, bool isFolder);
 }
