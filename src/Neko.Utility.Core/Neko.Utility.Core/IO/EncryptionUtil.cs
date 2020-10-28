663bc66862ff9e9f76b77876044836fd8ec434c4
SKYC829 <13677727680@163.com>
2020-10-28 17:03:27 +0800

    新增加密帮助类
    支持的加密 AES、DES、MD5、SHA1、SHA256、RSA<br/>支持的解密 AES、DES

---
+++ src/Neko.Utility.Core/Neko.Utility.Core/IO/EncryptionUtil.cs
@@ -0,0 +1,405 @@
+﻿using Neko.Utility.Core.Common;
+using Neko.Utility.Core.Data;
+using Neko.Utility.Core.IO.Logging;
+using System;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Neko.Utility.Core.IO
+{
+    /// <summary>
+    /// 加密帮助类
+    /// <para>一些常用的加密的快速操作</para>
+    /// </summary>
+    public sealed partial class EncryptionUtil
+    {
+        /// <summary>
+        /// 验证密钥长度,如果大于长度则会截断密钥
+        /// </summary>
+        /// <param name="keyBytes">密钥二进制数组</param>
+        /// <param name="keyLength">密钥长度</param>
+        /// <returns></returns>
+        private static byte[] VerifyKeyBytes(byte[] keyBytes, int keyLength)
+        {
+            if (keyLength <= 0)
+            {
+                return keyBytes;
+            }
+            byte[] result = new byte[keyLength];
+            try
+            {
+                if (keyBytes.Length > keyLength)
+                {
+                    Buffer.BlockCopy(keyBytes, 0, result, 0, keyLength);
+                }
+                else
+                {
+                    Buffer.BlockCopy(keyBytes, 0, result, 0, keyBytes.Length);
+                }
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                LogUtil.WriteException(ex, "在验证密钥时发剩错误,索引超出密钥长度,返回密钥本身!");
+                result = keyBytes;
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// 根据<see cref="ICryptoTransform"/>工厂进行对应的加密或解密操作
+        /// </summary>
+        /// <param name="cryptoTransform">加密或解密的工厂类</param>
+        /// <param name="value">要加密或解密的字节数据</param>
+        /// <returns></returns>
+        public static byte[] Encrypt(ICryptoTransform cryptoTransform, byte[] value)
+        {
+            byte[] result = null;
+            try
+            {
+                result = cryptoTransform.TransformFinalBlock(value, 0, value.Length);
+            }
+            catch (CryptographicException ex)
+            {
+                LogUtil.WriteException(ex);
+                throw;
+            }
+            finally
+            {
+                cryptoTransform.Dispose();
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// 创建加密解密用的<see cref="SymmetricAlgorithm"/>对象并设置密钥
+        /// </summary>
+        /// <typeparam name="TFactory">加密解密对象工厂</typeparam>
+        /// <param name="privateKey">密钥</param>
+        /// <param name="keyLength">密钥长度</param>
+        /// <returns></returns>
+        private static SymmetricAlgorithm GetEncryptFactory<TFactory>(string privateKey, int keyLength) where TFactory : SymmetricAlgorithm, new()
+        {
+            SymmetricAlgorithm managed = new TFactory();
+            managed.Mode = CipherMode.CBC;
+            managed.Padding = PaddingMode.PKCS7;
+            if (StringUtil.IsNullOrEmpty(privateKey))
+            {
+                privateKey = "19980522";
+            }
+            byte[] keyBytes = Encoding.Default.GetBytes(privateKey);
+            managed.Key = VerifyKeyBytes(keyBytes, keyLength);
+            managed.IV = VerifyKeyBytes(keyBytes, keyLength);
+            return managed;
+        }
+
+        /// <summary>
+        /// <inheritdoc cref="EncryptAES(object, string)"/>并转换为Base64字符串
+        /// </summary>
+        /// <param name="content">要加密的内容,如果是一个对象必须要实现<see cref="SerializableAttribute"/></param>
+        /// <param name="privateKey">加密的密钥</param>
+        /// <returns></returns>
+        public static string EncryptAESToString(object content, string privateKey)
+        {
+            byte[] encryptResult = EncryptAES(content, privateKey);
+            if (encryptResult == null || encryptResult.Length <= 0)
+            {
+                return string.Empty;
+            }
+            return Convert.ToBase64String(encryptResult);
+        }
+
+        /// <summary>
+        /// AES对称加密
+        /// </summary>
+        /// <param name="content">要加密的内容,如果是一个对象必须要实现<see cref="SerializableAttribute"/></param>
+        /// <param name="privateKey">加密的密钥</param>
+        /// <returns></returns>
+        public static byte[] EncryptAES(object content, string privateKey)
+        {
+            byte[] result = new byte[0];
+            if (StringUtil.IsNullOrEmpty(content))
+            {
+                return result;
+            }
+            RijndaelManaged managed = (RijndaelManaged)GetEncryptFactory<RijndaelManaged>(privateKey, 16);
+            result = Encrypt(managed.CreateEncryptor(), SerializeUtil.ToBinary(content));
+            managed.Dispose();
+            return result;
+        }
+
+        /// <summary>
+        /// <inheritdoc cref="DeEncryptAES(byte[], string)"/>
+        /// </summary>
+        /// <typeparam name="Tobject">对象类型</typeparam>
+        /// <param name="encryptContent">加密后的Base64字符串</param>
+        /// <param name="privateKey">加密的密钥</param>
+        /// <returns></returns>
+        public static Tobject DeEncryptAES<Tobject>(string encryptContent, string privateKey)
+        {
+            Tobject result = default(Tobject);
+            if (StringUtil.IsNullOrEmpty(encryptContent))
+            {
+                return result;
+            }
+            try
+            {
+                byte[] contentBytes = Convert.FromBase64String(encryptContent);
+                object deencryptResult = DeEncryptAES(contentBytes, privateKey);
+                Type resultType = deencryptResult.GetType();
+                if (resultType.GetInterface(nameof(IConvertible)) != null)
+                {
+                    result = (Tobject)Convert.ChangeType(deencryptResult, typeof(Tobject));
+                }
+                else
+                {
+                    result = (Tobject)deencryptResult;
+                }
+            }
+            catch (InvalidCastException ex)
+            {
+                LogUtil.WriteException(ex, "解密时无法转换对象类型!");
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// AES对称解密
+        /// </summary>
+        /// <param name="encryptBytes">要解密的二进制数据,如果是一个对象,该对象必须实现了<see cref="SerializableAttribute"/></param>
+        /// <param name="privateKey">加密的私钥</param>
+        /// <returns></returns>
+        public static object DeEncryptAES(byte[] encryptBytes, string privateKey)
+        {
+            object result = null;
+            if (StringUtil.IsNullOrEmpty(encryptBytes))
+            {
+                return result;
+            }
+            RijndaelManaged managed = (RijndaelManaged)GetEncryptFactory<RijndaelManaged>(privateKey, 16);
+            try
+            {
+                byte[] deencryptResult = Encrypt(managed.CreateDecryptor(), encryptBytes);
+                result = SerializeUtil.FromBinary<object>(deencryptResult);
+            }
+            catch (CryptographicException ex)
+            {
+                LogUtil.WriteException(ex, "解密失败");
+                throw ex;
+            }
+            catch (SerializationException ex)
+            {
+                LogUtil.WriteException(ex, "解密时序列化失败,无法反序列化对象");
+                throw ex;
+            }
+            finally
+            {
+                managed.Dispose();
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// <inheritdoc cref="EncryptDES(object, string)"/>并转换为Base64字符串
+        /// </summary>
+        /// <param name="content">要加密的内容,如果是一个对象必须要实现<see cref="SerializableAttribute"/></param>
+        /// <param name="privateKey">加密的密钥</param>
+        /// <returns></returns>
+        public static string EncryptDESToString(object content, string privateKey)
+        {
+            byte[] encryptResult = EncryptDES(content, privateKey);
+            if (encryptResult == null || encryptResult.Length <= 0)
+            {
+                return string.Empty;
+            }
+            return Convert.ToBase64String(encryptResult);
+        }
+
+        /// <summary>
+        /// DES对称加密
+        /// </summary>
+        /// <param name="content">要加密的内容,如果是一个对象必须要实现<see cref="SerializableAttribute"/></param>
+        /// <param name="privateKey">加密的密钥</param>
+        /// <returns></returns>
+        public static byte[] EncryptDES(object content, string privateKey)
+        {
+            byte[] result = new byte[0];
+            if (StringUtil.IsNullOrEmpty(content))
+            {
+                return result;
+            }
+            DESCryptoServiceProvider provider = (DESCryptoServiceProvider)GetEncryptFactory<DESCryptoServiceProvider>(privateKey, 8);
+            result = Encrypt(provider.CreateEncryptor(), SerializeUtil.ToBinary(content));
+            provider.Dispose();
+            return result;
+        }
+
+        /// <summary>
+        /// <inheritdoc cref="DeEncryptDES(byte[], string)"/>
+        /// </summary>
+        /// <typeparam name="Tobject">对象类型</typeparam>
+        /// <param name="encryptContent">要解密的数据,如果是一个对象,该对象必须实现了<see cref="SerializableAttribute"/></param>
+        /// <param name="privateKey">加密的私钥</param>
+        /// <returns></returns>
+        public static Tobject DeEncryptDES<Tobject>(string encryptContent, string privateKey)
+        {
+            Tobject result = default(Tobject);
+            if (StringUtil.IsNullOrEmpty(encryptContent))
+            {
+                return result;
+            }
+            try
+            {
+                byte[] contentBytes = Convert.FromBase64String(encryptContent);
+                object deencryptResult = DeEncryptDES(contentBytes, privateKey);
+                Type resultType = deencryptResult.GetType();
+                if (resultType.GetInterface(nameof(IConvertible)) != null)
+                {
+                    result = (Tobject)Convert.ChangeType(deencryptResult, typeof(Tobject));
+                }
+                else
+                {
+                    result = (Tobject)deencryptResult;
+                }
+            }
+            catch (InvalidCastException ex)
+            {
+                LogUtil.WriteException(ex, "解密时无法转换对象类型!");
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// DES对称解密
+        /// </summary>
+        /// <param name="encryptBytes">要解密的二进制数据,如果是一个对象,该对象必须实现了<see cref="SerializableAttribute"/></param>
+        /// <param name="privateKey">加密的私钥</param>
+        /// <returns></returns>
+        public static object DeEncryptDES(byte[] encryptBytes, string privateKey)
+        {
+            object result = null;
+            if (StringUtil.IsNullOrEmpty(encryptBytes))
+            {
+                return result;
+            }
+            DESCryptoServiceProvider provider = (DESCryptoServiceProvider)GetEncryptFactory<DESCryptoServiceProvider>(privateKey, 8);
+            try
+            {
+                byte[] deencryptResult = Encrypt(provider.CreateDecryptor(), encryptBytes);
+                result = SerializeUtil.FromBinary<object>(deencryptResult);
+            }
+            catch (CryptographicException ex)
+            {
+                LogUtil.WriteException(ex, "解密失败");
+                throw ex;
+            }
+            catch (SerializationException ex)
+            {
+                LogUtil.WriteException(ex, "解密时序列化失败,无法反序列化对象");
+                throw ex;
+            }
+            finally
+            {
+                provider.Dispose();
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// MD5加密
+        /// </summary>
+        /// <param name="content">要加密的内容,如果是一个对象必须要实现<see cref="SerializableAttribute"/></param>
+        /// <returns></returns>
+        public static string EncryptMD5(object content)
+        {
+            if (StringUtil.IsNullOrEmpty(content))
+            {
+                return string.Empty;
+            }
+            MD5CryptoServiceProvider provider = new MD5CryptoServiceProvider();
+            string result = string.Empty;
+            try
+            {
+                byte[] resultBytes = provider.ComputeHash(SerializeUtil.ToBinary(content));
+                for (int i = 0; i < resultBytes.Length; i++)
+                {
+                    result += resultBytes.ElementAt(i).ToString("x2");
+                }
+            }
+            catch (Exception ex)
+            {
+                LogUtil.WriteException(ex);
+            }
+            finally
+            {
+                provider.Dispose();
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// SHA256加密
+        /// </summary>
+        /// <param name="content">要加密的内容,如果是一个对象必须要实现<see cref="SerializableAttribute"/></param>
+        /// <returns></returns>
+        public static string EncryptSHA256(object content)
+        {
+            if (StringUtil.IsNullOrEmpty(content))
+            {
+                return string.Empty;
+            }
+            SHA256 managed = SHA256.Create();
+            string result = string.Empty;
+            try
+            {
+                byte[] resultBytes = managed.ComputeHash(SerializeUtil.ToBinary(content));
+                for (int i = 0; i < resultBytes.Length; i++)
+                {
+                    result += resultBytes.ElementAt(i).ToString("x2");
+                }
+            }
+            catch (Exception ex)
+            {
+                LogUtil.WriteException(ex);
+            }
+            finally
+            {
+                managed.Dispose();
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// SHA1加密
+        /// </summary>
+        /// <param name="content">要加密的内容,如果是一个对象必须要实现<see cref="SerializableAttribute"/></param>
+        /// <returns></returns>
+        public static string EncryptSHA1(object content)
+        {
+            if (StringUtil.IsNullOrEmpty(content))
+            {
+                return string.Empty;
+            }
+            SHA1 managed = SHA1.Create();
+            string result = string.Empty;
+            try
+            {
+                byte[] resultBytes = managed.ComputeHash(SerializeUtil.ToBinary(content));
+                for (int i = 0; i < resultBytes.Length; i++)
+                {
+                    result += resultBytes.ElementAt(i).ToString("x2");
+                }
+            }
+            catch (Exception ex)
+            {
+                LogUtil.WriteException(ex);
+            }
+            finally
+            {
+                managed.Dispose();
+            }
+            return result;
+        }
+    }
+}
---
+++ src/Neko.Utility.Core/Neko.Utility.Core/IO/EncryptionUtil_RSA.cs
@@ -0,0 +1,169 @@
+﻿using Neko.Utility.Core.Common;
+using Neko.Utility.Core.Data;
+using Neko.Utility.Core.IO.Logging;
+using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace Neko.Utility.Core.IO
+{
+    /*
+     * RSA加密解密类参考至以下文档:
+     * https://www.jb51.net/article/62858.htm => 原生的加密解密方式,加密长度有限制
+     * https://blog.csdn.net/weixin_38211198/article/details/107404282 => 修改后的加密解密方式,加密长度无限制,使用此方法不知道为什么可以作到私钥加密公钥解密,也许是因为我的私钥和公钥都是直接生成的,不是通过像是证书那样计算的原因
+     */
+    public sealed partial class EncryptionUtil
+    {
+        /// <summary>
+        /// 生成一对随机的公钥和私钥
+        /// </summary>
+        /// <returns>
+        /// <see cref="ValueTuple{T1,T2}.Item1"/>是私钥<br/><see cref="ValueTuple{T1,T2}.Item2"/>是公钥
+        /// </returns>
+        public static ValueTuple<string, string> GeneralRSAKey()
+        {
+            RSACryptoServiceProvider provider = new RSACryptoServiceProvider();
+            return new ValueTuple<string, string>(provider.ToXmlString(true), provider.ToXmlString(false));
+        }
+
+        /// <summary>
+        /// <inheritdoc cref="EncryptRSA(object, string)"/>并转换为Base64字符串
+        /// </summary>
+        /// <param name="content">要加密的内容,如果是一个对象必须要实现<see cref="SerializableAttribute"/></param>
+        /// <param name="publicKey">RSA公钥(因为C#原生的只支持公钥加密私钥解密,就假装这是私钥好了)</param>
+        /// <returns></returns>
+        public static string EncryptRSAToString(object content, string publicKey)
+        {
+            byte[] encryptResult = EncryptRSA(content, publicKey);
+            return Convert.ToBase64String(encryptResult);
+        }
+
+        /// <summary>
+        /// RSA非对称加密
+        /// </summary>
+        /// <param name="content">要加密的内容,如果是一个对象必须要实现<see cref="SerializableAttribute"/></param>
+        /// <param name="publicKey">RSA公钥(因为C#原生的只支持公钥加密私钥解密,就假装这是私钥好了)</param>
+        /// <returns></returns>
+        public static byte[] EncryptRSA(object content, string publicKey)
+        {
+            if (StringUtil.IsNullOrEmpty(content) || StringUtil.IsNullOrEmpty(publicKey))
+            {
+                return null;
+            }
+            RSACryptoServiceProvider provider = new RSACryptoServiceProvider();
+            provider.FromXmlString(publicKey);
+            byte[] result = new byte[0];
+            try
+            {
+                int bufferSize = (provider.KeySize / 8) - 11;
+                byte[] buffer = new byte[bufferSize];
+                using (MemoryStream inputStream = new MemoryStream(SerializeUtil.ToBinary(content)))
+                {
+                    using (MemoryStream outputStream = new MemoryStream())
+                    {
+                        while (inputStream.Position < inputStream.Length)
+                        {
+                            int readNum = inputStream.Read(buffer, 0, buffer.Length);
+                            byte[] encryptBlock = new byte[readNum];
+                            Buffer.BlockCopy(buffer, 0, encryptBlock, 0, readNum);
+                            byte[] encryptResult = provider.Encrypt(encryptBlock, false);
+                            outputStream.Write(encryptResult, 0, encryptResult.Length);
+                        }
+                        result = outputStream.ToArray();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                LogUtil.WriteException(ex);
+            }
+            finally
+            {
+                provider.Dispose();
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// <inheritdoc cref="DeEncryptRSA(byte[], string)"/>
+        /// </summary>
+        /// <typeparam name="Tobject">对象类型</typeparam>
+        /// <param name="encryptContent">要解密的数据,如果是一个对象,该对象必须实现了<see cref="SerializableAttribute"/></param>
+        /// <param name="privateKey">RSA私钥(因为C#原生的只支持公钥加密私钥解密,就假装这是公钥好了)</param>
+        /// <returns></returns>
+        public static Tobject DeEncryptRSA<Tobject>(string encryptContent, string privateKey)
+        {
+            Tobject result = default(Tobject);
+            if (StringUtil.IsNullOrEmpty(encryptContent) || StringUtil.IsNullOrEmpty(privateKey))
+            {
+                return result;
+            }
+            try
+            {
+                byte[] contentBytes = Convert.FromBase64String(encryptContent);
+                object deencryptResult = DeEncryptRSA(contentBytes, privateKey);
+                Type resultType = deencryptResult.GetType();
+                if (resultType.GetInterface(nameof(IConvertible)) != null)
+                {
+                    result = (Tobject)Convert.ChangeType(deencryptResult, typeof(Tobject));
+                }
+                else
+                {
+                    result = (Tobject)deencryptResult;
+                }
+            }
+            catch (InvalidCastException ex)
+            {
+                LogUtil.WriteException(ex, "解密时无法转换对象类型!");
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// RSA非对称解密
+        /// </summary>
+        /// <param name="encryptBytes">要解密的二进制数据,如果是一个对象,该对象必须实现了<see cref="SerializableAttribute"/></param>
+        /// <param name="privateKey">RSA私钥(因为C#原生的只支持公钥加密私钥解密,就假装这是公钥好了)</param>
+        /// <returns></returns>
+        public static object DeEncryptRSA(byte[] encryptBytes, string privateKey)
+        {
+            if (StringUtil.IsNullOrEmpty(encryptBytes) || StringUtil.IsNullOrEmpty(privateKey))
+            {
+                return null;
+            }
+            RSACryptoServiceProvider provider = new RSACryptoServiceProvider();
+            provider.FromXmlString(privateKey);
+            object result = null;
+            try
+            {
+                int bufferSize = (provider.KeySize / 8)/* - 11*/;
+                byte[] buffer = new byte[bufferSize];
+                using (MemoryStream inputStream = new MemoryStream(encryptBytes))
+                {
+                    using (MemoryStream outputStream = new MemoryStream())
+                    {
+                        while (inputStream.Position < inputStream.Length)
+                        {
+                            int readNum = inputStream.Read(buffer, 0, buffer.Length);
+                            byte[] encryptBlock = new byte[readNum];
+                            Buffer.BlockCopy(buffer, 0, encryptBlock, 0, readNum);
+                            byte[] deencryptResult = provider.Decrypt(encryptBlock, false);
+                            outputStream.Write(deencryptResult, 0, deencryptResult.Length);
+                        }
+                        byte[] binaryBytes = outputStream.ToArray();
+                        result = SerializeUtil.FromBinary(binaryBytes);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                LogUtil.WriteException(ex);
+            }
+            finally
+            {
+                provider.Dispose();
+            }
+            return result;
+        }
+    }
+}
